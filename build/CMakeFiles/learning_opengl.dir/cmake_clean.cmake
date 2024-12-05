eef152baafff06541fafdb72300bb48fbbb9ac61
alec <aleeeecyt@gmail.com>
2024-12-05 15:53:06 -0500

    it works

---
+++ build/CMakeFiles/learning_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_opengl.dir/link.d"
+  "CMakeFiles/learning_opengl.dir/include/glad.c.o"
+  "CMakeFiles/learning_opengl.dir/include/glad.c.o.d"
+  "CMakeFiles/learning_opengl.dir/src/main.cpp.o"
+  "CMakeFiles/learning_opengl.dir/src/main.cpp.o.d"
+  "learning_opengl"
+  "learning_opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/learning_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
+#include "../include/glad.h"
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
-    int a = 1;
-    std::cout << a << "goof" << std::endl;
-    return 0;
+
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "test NOW", NULL, NULL);
+	if(window == NULL) {
+		std::cout << "whoopsies couldnt make the window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "whoopsies couldnt make glad so i guess im sad :(" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
